749723c42e489a0ee2d00b5f969521ed81310b30
Inseok Son <inseokson92@gmail.com>
2023-07-24 22:32:21 +0900

    Change variable name(dropout_prob to p_dropout)

--- src/feed_forward.py
+++ src/feed_forward.py
@@ -3,12 +3,12 @@ from torch import nn
 
 
 class PositionWiseFeedForward(nn.Module):
-    def __init__(self, d_model: int, d_ff: int, activation: nn.Module = nn.ReLU, dropout_prob: float = 0.0):
+    def __init__(self, d_model: int, d_ff: int, activation: nn.Module = nn.ReLU, p_dropout: float = 0.0):
         super().__init__()
         self.linear1 = nn.Linear(d_model, d_ff)
         self.linear2 = nn.Linear(d_ff, d_model)
         self.activation = activation()
-        self.dropout = nn.Dropout(dropout_prob)
+        self.dropout = nn.Dropout(p_dropout)
 
     def forward(self, x: torch.Tensor):
         out = self.linear1(x)
